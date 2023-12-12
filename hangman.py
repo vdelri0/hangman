f43c7e63f69d9d6d84ee6546dc16907ca2ad61d0
victorprens <vprens12@gmail.com>
2023-12-12 18:56:35 -0500

    Hangman step 3.

--- hangman.py
+++ hangman.py
@@ -8,17 +8,15 @@ print(f"Chosen word: {chosen_word}\n")
 for _ in range(len(chosen_word)):
     display.append("_")
 
-guess = input("Guess a letter: ").lower()
+#TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
+
+while "_" in display:
+    guess = input("Guess a letter: ").lower()
 
 
-#TODO-2: - Loop through each position in the chosen_word;
-#If the letter at that position matches 'guess' then reveal that letter in the display at that position.
-#e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
-for i, letter in enumerate(chosen_word):
-    if letter == guess:
-        display[i] = guess
+    for i, letter in enumerate(chosen_word):
+        if letter == guess:
+            display[i] = guess
 
-#TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
-#Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
 
-print(display)
+    print(display)
