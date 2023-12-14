62d19e53af43e7c32b378ae4bc043c433aa78317
victorprens <vprens12@gmail.com>
2023-12-14 15:07:48 -0500

    Hangman step 4. (Unresolved)

--- hangman.py
+++ hangman.py
@@ -1,22 +1,92 @@
 import random
 
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
 display = []
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 print(f"Chosen word: {chosen_word}\n")
 
+#TODO-1: - Create a variable called 'lives' to keep track of the number of lives left.
+#Set 'lives' to equal 6.
+
+# Create blanks
 for _ in range(len(chosen_word)):
     display.append("_")
-
-#TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
 
 while "_" in display:
     guess = input("Guess a letter: ").lower()
 
-
+    # Check guessed letter
     for i, letter in enumerate(chosen_word):
         if letter == guess:
             display[i] = guess
 
+    #TODO-2: - If guess is not a letter in the chosen_word,
+    #Then reduce 'lives' by 1. 
+    #If lives goes down to 0 then the game should stop and it should print "You lose."
 
-    print(display)
+    #Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+
+    #Check if user has got all letters.
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    #TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
