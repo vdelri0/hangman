ae30e7fafaccf9e76fff130b4bbdbbaaad67dd22
victorprens <vprens12@gmail.com>
2023-12-14 15:36:37 -0500

    Hangman step 4.

--- hangman.py
+++ hangman.py
@@ -57,19 +57,18 @@ stages = ['''
 =========
 ''']
 
-display = []
+end_of_game = False
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
-print(f"Chosen word: {chosen_word}\n")
 
-#TODO-1: - Create a variable called 'lives' to keep track of the number of lives left.
-#Set 'lives' to equal 6.
+lives = 6
 
 # Create blanks
+display = []
 for _ in range(len(chosen_word)):
     display.append("_")
 
-while "_" in display:
+while not end_of_game:
     guess = input("Guess a letter: ").lower()
 
     # Check guessed letter
@@ -77,9 +76,12 @@ while "_" in display:
         if letter == guess:
             display[i] = guess
 
-    #TODO-2: - If guess is not a letter in the chosen_word,
-    #Then reduce 'lives' by 1. 
-    #If lives goes down to 0 then the game should stop and it should print "You lose."
+    # Guess is not a letter in chosen_word
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print(f"You Lose T_T")
 
     #Join all the elements in the list and turn it into a String.
     print(f"{' '.join(display)}")
@@ -89,4 +91,4 @@ while "_" in display:
         end_of_game = True
         print("You win.")
 
-    #TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
+    print(stages[lives])
